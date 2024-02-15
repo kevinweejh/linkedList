2359b1c10e85b5c3e0a40424105094142e93a4c3
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-15 21:07:37 +0800

    feat: add append method

--- linkedList.js
+++ linkedList.js
@@ -4,4 +4,19 @@ export class LinkedList {
     constructor() {
         this.head = null;
     }
+    
+    append = (value) => {  
+        // Set LinkedList head to point to new Node if LinkedList empty
+        if (this.head === null) {
+            this.head = new Node(value);
+            return;
+        } 
+
+        // Traverse LinkedList if not empty, set LinekdList tail to point to new Node
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = new Node(value);
+    }
 }
